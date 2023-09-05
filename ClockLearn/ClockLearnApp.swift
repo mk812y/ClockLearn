21e7fb8f85d7858dc4e36d2752925ab4cae833a5
mk812y <mk812y@gmail.com>
2023-09-05 20:58:40 +0200

    Initial Commit

---
+++ ClockLearn/ClockLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockLearnApp.swift
+//  ClockLearn
+//
+//  Created by Михаил Куприянов on 5.9.23..
+//
+
+import SwiftUI
+
+@main
+struct ClockLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
